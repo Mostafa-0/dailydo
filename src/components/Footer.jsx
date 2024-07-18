8aa6549bd04dc33161b58a00f561a1541c218786
Mostafa <mostafaahmed099@gmail.com>
2024-07-18 10:05:44 +0300

    edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,9 @@ import AuthContext from "../context/AuthContext";
 function Footer() {
   const { currentUser, loading } = useContext(AuthContext);
   return (
-    currentUser && !loading && (
-      <footer className="hidden md:block p-6 bg-neutral-300 bg-opacity-40 dark:bg-neutral-950 dark:bg-opacity-60 text-sm tracking-wider w-full">
+    currentUser &&
+    !loading && (
+      <footer className="p-6 bg-neutral-300 bg-opacity-40 dark:bg-neutral-950 dark:bg-opacity-60 text-sm tracking-wider w-full">
         <p>&#169; 2024 DailyDo. All Rights Reserved. By Mostafa Hashem</p>
       </footer>
     )
